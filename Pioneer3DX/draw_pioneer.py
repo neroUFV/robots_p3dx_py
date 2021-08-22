8562455120632ccaeb747d176a034081f40b37f3
João Victor Moreira <joaovictorma21@gmail.com>
2021-08-22 19:00:53 -0300

    Turning Pioneer simulation dynamic

--- Pioneer3DX/draw_pioneer.py
+++ Pioneer3DX/draw_pioneer.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+import matplotlib.animation as animation
 
 fig_1 = plt.figure(1, figsize=[5, 5])
-pts = np.array([[0, 2*(np.sqrt(1**2 - 0.5**2))/3], [-0.5, -(np.sqrt(1**2 - 0.5**2)) / 3], [0.5, -(np.sqrt(1**2 - 0.5**2))/3]])
-p = Polygon(pts, closed=False)
+triangle_points = np.array([[0, 2*(np.sqrt(1**2 - 0.5**2))/3], [-0.5, -(np.sqrt(1**2 - 0.5**2)) / 3], [0.5, -(np.sqrt(1**2 - 0.5**2))/3]])
+p = Polygon(triangle_points, closed=False)
 p.set_color('r')
 grid_1 = fig_1.add_subplot()
 grid_1.add_patch(p)
@@ -13,6 +14,25 @@ plt.ylabel("y")
 plt.axis([-5, 5, -5, 5])
 plt.grid()
 plt.title("Pioneer P3DX - Simulator")
+
+
+def init():
+    return p,
+
+
+def animate1(i):
+    triangle_points[:, 1] += i
+    print(triangle_points)
+    p.set_xy(triangle_points)
+    return p,
+
+
+ani = animation.FuncAnimation(fig_1, animate1,
+                              frames=np.arange(0, 10, 0.0001),
+                              init_func=init,
+                              interval=10,
+                              blit=True)
+
 plt.show()
 
 """
